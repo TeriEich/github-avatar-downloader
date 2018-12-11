98e0fc82052984d2865ff73aeef83264abee291c
Teri <teri.eich@gmail.com>
2018-12-11 01:22:26 +0000

    modified functions to return a list of avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,32 @@
 var request = require('request');
+var authorization = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': authorization
     }
   };
 
-  request(url, function(err, res, body) {
-    cb(err, body);
+  // var obj = {};
+  // return cb(obj);
+
+  request(options, function(err, res, body) {
+    let obj = JSON.parse(body);
+  // let obj = JSON.stringify(body);
+  // obj = JSON.parse(obj);
+    cb(err, obj);
   });
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
-  console.log("Errors: ", err);
-  console.log("Result: ", result);
+  for (let url in result) {
+    console.log(result[url].avatar_url);
+  }
+  // console.log("Errors: ", err);
+  // console.log("Result: ", result);
 });
