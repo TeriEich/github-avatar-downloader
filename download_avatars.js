51530ee722a920a93d0f7d97d27548292bd6a9fc
Teri <teri.eich@gmail.com>
2018-12-16 02:27:47 +0000

    added ability to input repo name and owner from the command line - took out the hardcoded repo

--- download_avatars.js
+++ download_avatars.js
@@ -1,39 +1,49 @@
-var request = require('request');
-var authorization = require('./secrets');
-var fs = require('fs');
+const request = require('request');
+const authorization = require('./secrets');
+const fs = require('fs');
+
+const userInput = process.argv.slice(2);
+console.log(userInput);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': authorization
-    }
-  };
-  console.log('Downloading file...');
+if (userInput[0] && userInput[1]) {
+  function getRepoContributors(repoOwner, repoName, cb) {
+    const options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': authorization.GITHUB_TOKEN
+      }
+    };
+    console.log('Downloading file...');
+    console.log(options.url);
 
-  request(options, function(err, res, body) {
-    let obj = JSON.parse(body);
-    cb(err, obj);
-  });
-}
-
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-  .pipe(fs.createWriteStream(filePath));
-}
-
-
-getRepoContributors('jquery', 'jquery', function(err, result) {
-  for (let url in result) {
-    let avatarURL = result[url].avatar_url;
-    let fileName = result[url].login;
-    downloadImageByURL(avatarURL, `./avatars/${fileName}.jpg`);
+    request(options, function(err, res, body) {
+      let obj = JSON.parse(body);
+      cb(err, obj);
+    });
   }
-  console.log("Errors: ", err);
-});
 
-console.log('Download complete.');
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .pipe(fs.createWriteStream(filePath));
+  }
+
+
+  getRepoContributors(userInput[1], userInput[0], function(err, result) {
+    for (let url in result) {
+      let avatarURL = result[url].avatar_url;
+      let fileName = result[url].login;
+      downloadImageByURL(avatarURL, `./avatars/${fileName}.jpg`);
+    }
+    console.log("Errors: ", err);
+    console.log('Download complete.');
+  });
+
+
+} else {
+  console.log('Error: Please input a repo and then the owner!');
+}
+
